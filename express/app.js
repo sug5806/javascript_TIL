7339aca05a7028e78ce819d56944412ee8204845
홍동수 <sug5806@naver.com>
2021-07-11 17:46:17 +0900

    html 서빙해보기

--- express/app.js
+++ express/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.set('port', 8080);
 app.get('/', (req, res) => {
-    res.send('hello express');
+    res.status(200);
+    res.sendFile(path.join(__dirname, 'index.html'))
 });
 
 app.get('/user', (req, res) => {
