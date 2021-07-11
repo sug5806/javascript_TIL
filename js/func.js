b9faa6b8eda4607505c42abbe836c3e3c77bb1ac
홍동수 <sug5806@naver.com>
2021-07-11 14:20:04 +0900

    checkOddOrEven 함수 추가 후 다시 exports시키기

--- js/func.js
+++ js/func.js
@@ -2,3 +2,17 @@ const {odd, even} = require('var');
 
 console.log(odd);
 console.log(even);
+
+function checkOddOrEven(number){
+    if (number % 2) {
+        return odd;
+    } else {
+        return even;
+    }
+}
+
+module.exports = {
+    checkOddOrEven,
+    odd,
+    even,
+};
