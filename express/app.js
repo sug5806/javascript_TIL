8c6fd2083da087c8893e156f58921b3d0c7f2651
홍동수 <sug5806@naver.com>
2021-07-12 11:38:21 +0900

    router 사용하기

--- express/app.js
+++ express/app.js
@@ -3,25 +3,29 @@ const path = require('path');
 
 const app = express();
 
+const router = express.Router();
+
 app.set('port', 8080);
 
-app.get('/category/:name', (req, res) => {
+router.get('/category/:name', (req, res) => {
     res.send(`hello ${req.params.name}`);
 });
 
-
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200);
     res.sendFile(path.join(__dirname, 'index.html'))
 });
 
-app.get('/user', (req, res) => {
+router.get('/user', (req, res) => {
     res.send('user user')
+});
+
+router.use((req, res, next) => {
+    res.status(404).send('page not found');
 })
 
-app.use((err, req, res, next) => {
-    console.log(err);
-    next();
+router.use((req, res, next) => {
+    console.log("asdf");
 })
 
 app.listen(app.get('port'), () => {
