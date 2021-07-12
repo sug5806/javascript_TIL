cc64419ffcb0d33f957c007aaf5b0708d2243dd9
홍동수 <sug5806@naver.com>
2021-07-12 11:08:05 +0900

    미들웨어 사용하기

--- express/app.js
+++ express/app.js
@@ -4,6 +4,13 @@ const path = require('path');
 const app = express();
 
 app.set('port', 8080);
+
+app.use((req, res, next) => {
+    console.log('모든 요청에 실행하고 싶어요');
+    next();
+});
+
+
 app.get('/', (req, res) => {
     res.status(200);
     res.sendFile(path.join(__dirname, 'index.html'))
