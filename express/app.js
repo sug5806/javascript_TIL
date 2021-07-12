c9fdd78c286c34a134151156c7a4e772b0cd4c14
홍동수 <sug5806@naver.com>
2021-07-12 14:07:46 +0900

    비밀키 관리를 위한 dotenv 추가

--- express/app.js
+++ express/app.js
@@ -1,17 +1,28 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
+dotenv.config();
 const app = express();
 
 const router = express.Router();
 
 app.set('port', 8080);
 app.use(morgan('dev'));
-app.use(cookieParser());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "secret",
+
+}))
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
 
 
 router.get('/category/:name', (req, res) => {
