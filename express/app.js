093b352aad77e7fdd29ab792f9542c8a582b9e19
홍동수 <sug5806@naver.com>
2021-07-12 14:14:28 +0900

    route 분리하기

--- express/app.js
+++ express/app.js
@@ -6,9 +6,11 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
 dotenv.config();
-const app = express();
 
-const router = express.Router();
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+
+const app = express();
 
 app.set('port', 8080);
 app.use(morgan('dev'));
@@ -17,19 +19,20 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     secret: "secret",
-
 }))
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+app.use('/', indexRouter);
+app.use('/user', userRouter);
 
 
-router.get('/category/:name', (req, res) => {
+app.get('/category/:name', (req, res) => {
     res.send(`hello ${req.params.name}`);
 });
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200);
 
     res.cookie('myasdf', 'asdf', {
@@ -38,15 +41,15 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 });
 
-router.get('/user', (req, res) => {
+app.get('/user', (req, res) => {
     res.send('user user')
 });
 
-router.get('/json', (req, res) => {
+app.get('/json', (req, res) => {
     res.json( {hello: 'express'})
 })
 
-app.use(router);
+app.use(app);
 
 app.listen(app.get('port'), () => {
     console.log('익스프레스 서버 실행');
