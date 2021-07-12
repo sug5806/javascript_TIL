db233bcf19e3d7e4fe49a3c008ac2324a04cf02d
홍동수 <sug5806@naver.com>
2021-07-12 12:31:44 +0900

    cookie-parser, morgan 사용

--- express/app.js
+++ express/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 const router = express.Router();
 
 app.set('port', 8080);
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
-router.use((req, res, next) => {
-    console.log('Time: ', Date.now());
-    next();
-})
 
 router.get('/category/:name', (req, res) => {
     res.send(`hello ${req.params.name}`);
@@ -18,6 +20,10 @@ router.get('/category/:name', (req, res) => {
 
 router.get('/', (req, res) => {
     res.status(200);
+
+    res.cookie('myasdf', 'asdf', {
+        httpOnly: true,
+    })
     res.sendFile(path.join(__dirname, 'index.html'))
 });
 
